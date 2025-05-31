7ad67e11a6844b50e438f563605d293a91251d74
manjunath031984 <85352670+manjunath031984@users.noreply.github.com>
2025-05-31 14:03:25 +0530

    Create variables.tf

---
+++ modules/r53/variables.tf
@@ -0,0 +1,15 @@
+variable "zone_id" {
+  type = string
+}
+
+variable "record_name" {
+  type = string
+}
+
+variable "alb_dns_name" {
+  type = string
+}
+
+variable "alb_zone_id" {
+  type = string
+}
